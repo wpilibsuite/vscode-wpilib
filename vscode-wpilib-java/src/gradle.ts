9ccc6cf5183d5efc65c6c92aaf05fe01479e28e0
Thad House <ThadHouse@users.noreply.github.com>
2018-03-20 21:42:49 -0700

    Fixes gradle running and the java build file.

--- vscode-wpilib-java/src/gradle.ts
+++ vscode-wpilib-java/src/gradle.ts
@@ -3,38 +3,41 @@ import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
 export interface OutputPair {
-  stdout: string;
-  stderr: string;
+    stdout: string;
+    stderr: string;
 }
 
-export function executeCommandAsync(command: string, rootDir: string, ow?: vscode.OutputChannel) : Promise<OutputPair> {
-  return new Promise(function (resolve, reject) {
-      const exec = child_process.exec;
-      const child = exec(command, {
-          cwd: rootDir
-      }, (err, stdout, stderr) => {
-          if (err) {
-              reject(err);
-          } else {
-              resolve({stdout: stdout, stderr: stderr});
-          }
-      });
+export function executeCommandAsync(command: string, rootDir: string, ow?: vscode.OutputChannel): Promise<OutputPair> {
+    return new Promise(function (resolve, reject) {
+        const exec = child_process.exec;
+        const child = exec(command, {
+            cwd: rootDir
+        }, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ stdout: stdout, stderr: stderr });
+            }
+        });
 
-      if (ow === undefined) {
-          return;
-      }
+        if (ow === undefined) {
+            return;
+        }
 
-      child.stdout.on('data', (data) => {
-          ow.append(data.toString());
-      });
+        child.stdout.on('data', (data) => {
+            ow.append(data.toString());
+        });
 
-      child.stderr.on('data', (data) => {
-          ow.append(data.toString());
-      });
-  });
+        child.stderr.on('data', (data) => {
+            ow.append(data.toString());
+        });
+    });
 }
 
 export async function gradleRun(args: string, rootDir: string, ow?: vscode.OutputChannel): Promise<OutputPair> {
-  const command = 'gradlew ' + args;
-  return await executeCommandAsync(command, rootDir, ow);
+    let command = 'gradlew ' + args;
+    if (process.platform !== 'win32') {
+        command = './' + command;
+    }
+    return await executeCommandAsync(command, rootDir, ow);
 }
