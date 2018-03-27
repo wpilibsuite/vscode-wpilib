25bf81b9df863f80d85ce3dedd22111f4f166ae0
Thad House <ThadHouse@users.noreply.github.com>
2018-03-26 22:05:31 -0700

    Adds devtool toggle to riolog

--- vscode/riolog-standalone/src/riolog/script/implscript.ts
+++ vscode/riolog-standalone/src/riolog/script/implscript.ts
@@ -1,6 +1,7 @@
 'use strict';
 
-let ipcRenderer = require('electron').ipcRenderer;
+const ipcRenderer = require('electron').ipcRenderer;
+const remote = require('electron').remote;
 
 import { checkResizeImpl, handleMessage } from '../shared/sharedscript';
 import { IIPCReceiveMessage, IIPCSendMessage } from '../shared/interfaces';
@@ -16,3 +17,11 @@ export function sendMessage(message: IIPCReceiveMessage) {
 ipcRenderer.on('messageFromMain', (_:any, data: IIPCSendMessage) => {
     handleMessage(data);
 });
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 123) {
+      remote.getCurrentWindow().webContents.toggleDevTools();
+    } else if (e.which === 116) {
+      location.reload();
+    }
+  });
