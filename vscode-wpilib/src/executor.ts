ada6909b630064a6ff53fda0597fd15115a49963
Thad House <ThadHouse@users.noreply.github.com>
2018-07-02 14:41:04 -0700

    Fixes execution on windows with any shell. Bumps gradlerio version.
    Closes #31

--- vscode-wpilib/src/executor.ts
+++ vscode-wpilib/src/executor.ts
@@ -2,6 +2,7 @@
 import * as vscode from 'vscode';
 import { IExecuteAPI } from './shared/externalapi';
 import { PromiseCondition } from './shared/promisecondition';
+import { getIsWindows } from './utilities';
 
 interface ITaskRunner {
   execution: vscode.TaskExecution;
@@ -36,6 +37,18 @@ export class ExecuteAPI extends IExecuteAPI {
     const shell = new vscode.ShellExecution(command, {
       cwd: rootDir,
     });
+
+    if (getIsWindows()) {
+      if (command.startsWith('./')) {
+        command = command.substring(2);
+      }
+      shell.commandLine = command;
+      if (shell.options !== undefined) {
+        shell.options.executable = 'cmd.exe';
+        shell.options.shellArgs = ['/d', '/c'];
+      }
+    }
+
     const task = new vscode.Task({ type: 'wpilibgradle' }, workspace, name, 'wpilib', shell);
     const execution = await vscode.tasks.executeTask(task);
     const runner: ITaskRunner = {
