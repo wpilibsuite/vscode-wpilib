a87b3c288b2550c667c99f26bcf75f5a4d7e6233
Thad House <thadhouse1@gmail.com>
2018-10-05 17:06:25 -0700

    Update frc home function to match new locations
    No more FRC environmental variable, instead uses %PUBLIC%\frc{year}

--- vscode-wpilib/src/utilitiesapi.ts
+++ vscode-wpilib/src/utilitiesapi.ts
@@ -6,24 +6,26 @@ import { IUtilitiesAPI } from 'vscode-wpilibapi';
 import { getIsWindows } from './utilities';
 
 export class UtilitiesAPI implements IUtilitiesAPI {
+  private wpilibHome: string | undefined;
+
   public getFrcYear(): string {
     return '2018';
   }
   public getWPILibHomeDir(): string {
-    const year = this.getFrcYear();
-    const frcHome = process.env[`FRC_${year}_HOME`];
-    if (frcHome) {
-      return frcHome;
-    } else {
-      if (getIsWindows()) {
-        // Windows, search public home
-        return '';
-      } else {
-        // Unix, search user home
-        const dir = os.homedir();
-        const wpilibhome = path.join(dir, `wpilib${year}`);
-        return wpilibhome;
-      }
+    if (this.wpilibHome) {
+      return this.wpilibHome;
     }
+    const year = this.getFrcYear();
+    if (getIsWindows()) {
+      let publicFolder = process.env.PUBLIC;
+      if (!publicFolder) {
+        publicFolder = 'C:\\Users\\Public';
+      }
+      this.wpilibHome = path.join(publicFolder, `frc${year}`);
+    } else {
+      const dir = os.homedir();
+      this.wpilibHome = path.join(dir, `frcd${year}`);
+    }
+    return this.wpilibHome;
   }
 }
