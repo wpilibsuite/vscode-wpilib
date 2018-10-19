0e111f9224e17a1861b6b8a28c8199707775693b
Thad House <thadhouse1@gmail.com>
2018-10-19 16:20:22 -0700

    Correctly makes webpack use node_modules files when needed
    Closes #107

--- vscode-wpilib/webpack.config.js
+++ vscode-wpilib/webpack.config.js
@@ -3,21 +3,29 @@ const path = require('path');
 module.exports = {
   mode: 'none',
   entry: {
-    eclipseimportpage: './src/webviews/pages/eclipseimportpage.ts',
-    projectcreatorpage: './src/webviews/pages/projectcreatorpage.ts',
+    //eclipseimportpage: './src/webviews/pages/eclipseimportpage.ts',
+    //projectcreatorpage: './src/webviews/pages/projectcreatorpage.ts',
     riologpage: './src/riolog/shared/sharedscript.ts'
   },
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
+        test: /\.ts$/,
         use: 'ts-loader',
-        exclude: '/node_modules/'
+      },
+      {
+        test: /\.js$/,
+        include: [/node_modules/],
       }
-    ]
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js']
+
+  },
+  node: {
+    net: 'empty',
   },
   output: {
     path: path.resolve(__dirname, 'resources',  'dist'),
