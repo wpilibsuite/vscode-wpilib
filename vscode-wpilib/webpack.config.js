14326d25a7e9059367083115d23009e374783c47
Thad House <thadhouse1@gmail.com>
2018-11-12 14:58:45 -0800

    Add eclipse and new project pages to webpack
    They were missing

--- vscode-wpilib/webpack.config.js
+++ vscode-wpilib/webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   mode: 'none',
   entry: {
-    //eclipseimportpage: './src/webviews/pages/eclipseimportpage.ts',
-    //projectcreatorpage: './src/webviews/pages/projectcreatorpage.ts',
+    eclipseimportpage: './src/webviews/pages/eclipseimportpage.ts',
+    projectcreatorpage: './src/webviews/pages/projectcreatorpage.ts',
     riologpage: './src/riolog/shared/sharedscript.ts'
   },
   devtool: 'inline-source-map',
