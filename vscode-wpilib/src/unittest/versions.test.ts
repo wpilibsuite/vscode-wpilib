b66d2c5c445b470f0c57194a5f20f7bac08ce50c
Thad House <thadhouse1@gmail.com>
2018-11-30 13:37:49 -0800

    Test equal cases are always false

--- vscode-wpilib/src/unittest/versions.test.ts
+++ vscode-wpilib/src/unittest/versions.test.ts
@@ -21,14 +21,12 @@ const data = [
   new TestData('5.10.0', '5.9.0', true),
   new TestData('5.9.0', '5.9.0-1', true),
   new TestData('5.9.0', '5.9', true),
-  new TestData('5.9', '5.9', false),
   new TestData('5.9.1', '5.9', true),
   new TestData('5.9', '5.9.0', false),
   new TestData('5.9', '5.9.1', false),
   new TestData('5.10.0', '5.9.0-1', true),
   new TestData('5.8.0', '5.9.0-1', false),
   new TestData('5.8.0-1', '5.8.0-2', false),
-  new TestData('5.8.0-2', '5.8.0-2', false),
   new TestData('2019.1.1', '2019.1.1-beta-1', true),
   new TestData('2019.1.1-beta-2', '2019.1.1-beta-1', true),
   new TestData('2019.1.1-beta-2b', '2019.1.1-beta-2', true),
@@ -36,13 +34,52 @@ const data = [
 ];
 
 suite('Version Tests', () => {
-
   data.forEach((x) => {
     test(`Version Test: a: ${x.a} b: ${x.b} result: ${x.result}`, () => {
       assert.strictEqual(isNewerVersion(x.a, x.b), x.result);
     });
-    test(`Negative Version Test: a: ${x.a} b: ${x.b} result: ${!x.result}`, () => {
+    test(`Invert Version Test: a: ${x.a} b: ${x.b} result: ${!x.result}`, () => {
       assert.strictEqual(!isNewerVersion(x.a, x.b), !x.result);
+    });
+    test(`Reverse Version Test: a: ${x.b} b: ${x.a} result: ${!x.result}`, () => {
+      assert.strictEqual(isNewerVersion(x.b, x.a), !x.result);
+    });
+    test(`Reverse Invert Version Test: a: ${x.b} b: ${x.a} result: ${x.result}`, () => {
+      assert.strictEqual(!isNewerVersion(x.b, x.a), x.result);
+    });
+  });
+});
+
+class EqualTestData {
+  public readonly a: string;
+  public readonly b: string;
+
+  constructor(a: string, b: string) {
+    this.a = a;
+    this.b = b;
+  }
+}
+
+const equalData = [
+  new EqualTestData('0', '0'),
+  new EqualTestData('0.0', '0.0'),
+  new EqualTestData('0.0.0', '0.0.0'),
+  new EqualTestData('0.0.0-0', '0.0.0-0'),
+];
+
+suite('Equal Version Tests', () => {
+  equalData.forEach((x) => {
+    test(`Version Test: a: ${x.a} b: ${x.b}`, () => {
+      assert.strictEqual(isNewerVersion(x.a, x.b), false);
+    });
+    test(`Invert Version Test: a: ${x.a} b: ${x.b}`, () => {
+      assert.strictEqual(!isNewerVersion(x.a, x.b), true);
+    });
+    test(`Reverse Version Test: a: ${x.b} b: ${x.a}`, () => {
+      assert.strictEqual(isNewerVersion(x.b, x.a), false);
+    });
+    test(`Reverse Invert Version Test: a: ${x.b} b: ${x.a}`, () => {
+      assert.strictEqual(!isNewerVersion(x.b, x.a), true);
     });
   });
 });
