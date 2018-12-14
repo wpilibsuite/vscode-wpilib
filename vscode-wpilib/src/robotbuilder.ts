5f55dba7cb1f69a7618e4aa75860a6124f04222a
Thad House <thadhouse1@gmail.com>
2018-12-14 09:47:32 -0800

    Add yaml generation

--- vscode-wpilib/src/robotbuilder.ts
+++ vscode-wpilib/src/robotbuilder.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 import { IExampleTemplateAPI, IExampleTemplateCreator } from 'vscode-wpilibapi';
 import { logger } from './logger';
 import { generateCopyCpp, generateCopyJava } from './shared/generator';
-import { extensionContext } from './utilities';
+import { extensionContext, promisifyReadFile, promisifyWriteFile } from './utilities';
 
 export async function addRobotBuilderExamples(resourceRoot: string, core: IExampleTemplateAPI) {
   const storagePath = extensionContext.storagePath;
@@ -13,6 +13,14 @@ export async function addRobotBuilderExamples(resourceRoot: string, core: IExamp
     return;
   }
   const gradleBasePath = path.join(resourceRoot, 'gradle');
+  const yamlFile = path.join(resourceRoot, 'robotbuilderbase.yaml');
+  let yamlText = '';
+  try {
+    yamlText = await promisifyReadFile(yamlFile);
+  } catch (err) {
+    logger.error('failed to find yaml file for rb', err);
+    return;
+  }
   const items = ['cpp', 'java'];
   for (const lang of items) {
     const provider: IExampleTemplateCreator = {
@@ -28,16 +36,25 @@ export async function addRobotBuilderExamples(resourceRoot: string, core: IExamp
       async generate(folderInto: vscode.Uri): Promise<boolean> {
         try {
           if (lang === 'java') {
+            const javaPackage = 'frc.robot';
             if (!await generateCopyJava(async () => {
+              const projectName = path.basename(folderInto.fsPath);
+              const replacedProject = yamlText.replace(new RegExp('###PACKAGEREPLACE###', 'g'), javaPackage)
+                                              .replace(new RegExp('###PROJECTNAMEREPLACE###', 'g'), projectName);
+              await promisifyWriteFile(path.join(folderInto.fsPath, projectName + '.yaml'), replacedProject);
               return true;
             },
-              path.join(gradleBasePath, 'java'), folderInto.fsPath, 'frc.robot.' + 'Main',
+              path.join(gradleBasePath, 'java'), folderInto.fsPath, javaPackage + '.Main',
               path.join('frc', 'robot'))) {
               vscode.window.showErrorMessage('Cannot create into non empty folder');
               return false;
             }
           } else {
             if (!await generateCopyCpp(async () => {
+              const projectName = path.basename(folderInto.fsPath);
+              const replacedProject = yamlText.replace(new RegExp('###PACKAGEREPLACE###', 'g'), 'cpp')
+                                              .replace(new RegExp('###PROJECTNAMEREPLACE###', 'g'), projectName);
+              await promisifyWriteFile(path.join(folderInto.fsPath, projectName + 'yaml'), replacedProject);
               return true;
             },
               path.join(gradleBasePath, 'cpp'), folderInto.fsPath, false)) {
