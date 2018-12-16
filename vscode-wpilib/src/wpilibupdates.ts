69266a9cd53c0b1a01bfbe1e5069be31b0c3a3b3
Thad House <ThadHouse@users.noreply.github.com>
2018-12-15 17:52:36 -0800

    Use Webpack to build the extension (#125)
    * Use Webpack to build the extension
    
    This speeds up loading and activation by a large amount.
    
    * Fix publish
    
    * Move nls to webpack
    
    * Add nls back in
    
    * Remove code NLS, fixes up webpack
    
    There was only 1 NLS location in code, and there likely were not going to be many more this year.
    
    The advantages of going to webpack are large, so that was the best plan

--- vscode-wpilib/src/wpilibupdates.ts
+++ vscode-wpilib/src/wpilibupdates.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import fetch from 'node-fetch';
+import * as fetch from 'node-fetch';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { IExternalAPI } from 'vscode-wpilibapi';
