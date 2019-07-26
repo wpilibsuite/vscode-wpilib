e58a42f54caff233a7a2a36935f6c03a45314af6
Thad House <thadhouse1@gmail.com>
2019-07-26 14:44:26 -0700

    Fix linting of vscode module

--- vscode-wpilib/src/test/index.ts
+++ vscode-wpilib/src/test/index.ts
@@ -11,13 +11,13 @@
 // a possible error to the callback or null if none.
 
 // tslint:disable-next-line:no-submodule-imports
-import * as testRunner from 'vscode/lib/testrunner';
+// import * as testRunner from 'vscode/lib/testrunner';
 
-// You can directly control Mocha options by uncommenting the following lines
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true, // colored output from test results
-});
+// // You can directly control Mocha options by uncommenting the following lines
+// // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+// testRunner.configure({
+//     ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
+//     useColors: true, // colored output from test results
+// });
 
-module.exports = testRunner;
+// module.exports = testRunner;
