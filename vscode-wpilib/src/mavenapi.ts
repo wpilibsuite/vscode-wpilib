1ede2efadc2d0e126d38872337af6474e84dee58
Thad House <ThadHouse@users.noreply.github.com>
2019-08-01 08:58:33 -0700

    Add support for displaying and downloading API docs (#254)

---
+++ vscode-wpilib/src/mavenapi.ts
@@ -0,0 +1,64 @@
+'use strict';
+
+import * as xml2js from 'xml2js';
+import { downloadTextFile } from './fetchhelpers';
+import { isNewerVersion } from './versions';
+
+export interface IVersions {
+  version: string[];
+}
+
+export interface IVersioning {
+  versions: IVersions[];
+}
+
+export interface IMetaData {
+ versioning: IVersioning[];
+ artifactId: string;
+}
+
+export interface IMavenMetaData {
+  metadata: IMetaData;
+}
+
+export function getMavenMetadataContents(repoRoot: string): Promise<string> {
+  const xmlFile = repoRoot[repoRoot.length - 1] === '/' ? repoRoot + 'maven-metadata.xml' : repoRoot + '/maven-metadata.xml';
+  return downloadTextFile(xmlFile);
+}
+
+export function getMavenMetadata(xmlFile: string): Promise<IMavenMetaData> {
+  return new Promise<IMavenMetaData>((resolve, reject) => {
+    xml2js.parseString(xmlFile, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      // tslint:disable-next-line:no-unsafe-any
+      resolve(result as IMavenMetaData);
+    });
+  });
+}
+
+export function getMavenVersions(metadata: IMavenMetaData): string[] {
+  return metadata.metadata.versioning[0].versions[0].version;
+}
+
+export function getNewestMavenVersion(versions: string[]): string | undefined {
+  if (versions.length === 0) {
+    return undefined;
+  }
+  let newestVersion = versions[0];
+  for (const v of versions) {
+    if (isNewerVersion(v, newestVersion)) {
+      newestVersion = v;
+    }
+  }
+  return newestVersion;
+}
+
+export function constructDownloadUrl(metadata: IMavenMetaData, repoRoot: string, version: string, ext: string) {
+  const rootDir = repoRoot[repoRoot.length - 1] === '/' ? repoRoot : repoRoot + '/';
+  const versionDir = rootDir + version + '/';
+  const fileDir = versionDir + metadata.metadata.artifactId + '-' + version + ext;
+  return fileDir;
+}
