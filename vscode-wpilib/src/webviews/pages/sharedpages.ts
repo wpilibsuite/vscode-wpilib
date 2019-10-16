dec8f9ba228f01e385a84637c3c20828e7f4ec50
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2019-10-16 13:27:47 +0800

    update project name and path valiadtion
    update project name and path valiadtion
    
    project name block strategies
    - string started with or ended with  symbols are not allowed
    - string started with or ended with space are not allowed
    - string with blacklist character
    
    path block strategy:
    - path with blacklist character

--- vscode-wpilib/src/webviews/pages/sharedpages.ts
+++ vscode-wpilib/src/webviews/pages/sharedpages.ts
@@ -5,12 +5,45 @@ declare global {
     interface Window { i18nTrans: (domain: string, message: string, ...args: any[]) => string; }
 }
 
+export function hasSpecialCharacter(str: string): boolean {
+  const characterBlacklist = ['@', '!', '.', '/', '\\', '$', '&', '|', '`', '~', ':'];
+  // check black list characters
+  for (const c of str) {
+    if (characterBlacklist.indexOf(c) !== -1) {
+      return true;
+    }
+  }
+  // check prefix and suffix
+  for (const c of (str[0] + str[str.length - 1])) {
+    if (c >= '0' && c <= '9') {
+      continue;
+    }
+    if (c >= 'A' && c <= 'Z') {
+      continue;
+    }
+    if (c >= 'a' && c <= 'z') {
+      continue;
+    }
+    return true;
+  }
+  return false;
+}
+
+export function validatePath(str: string): boolean {
+  const characterBlacklist = ['@', '!', '.', '$', '&', '|', '`'];
+  for (const c of str) {
+    if(characterBlacklist.indexOf(c) !== -1) {
+      return false;
+    }
+  } 
+  return true;
+}
+
 export function validateProject() {
   const elem = document.getElementById('projectName') as HTMLButtonElement;
   const s = elem.value;
-  const match = s.match(/\w[\w-]*$/gm);
   const pdiv = document.getElementById('projectnamediv') as HTMLDivElement;
-  if (match === null || match.length === 0) {
+  if ( hasSpecialCharacter(s)) {
     pdiv.innerText = window.i18nTrans('ui', 'Invalid project name');
     pdiv.classList.add('error');
     elem.classList.add('error');
@@ -34,5 +67,20 @@ export function validateTeamNumber() {
     pdiv.innerText = window.i18nTrans('ui', 'Enter a team number');
     pdiv.classList.remove('error');
     elem.classList.remove('error');
+  }
+}
+
+export function validateProjectFolder() {
+  const input = document.getElementById('projectFolder') as HTMLInputElement;
+  const div = document.getElementById('projectFolderdiv') as HTMLInputElement;
+  const s = input.value;
+  if (!validatePath(s)) {
+    div.innerText = 'Select a folder to place the new project into. path includes illegal character(s)';
+    div.classList.add('error');
+    input.classList.add('error');
+  } else {
+    div.innerText = 'Select a folder to place the new project into.';
+    div.classList.remove('error');
+    input.classList.remove('error');
   }
 }
--- wpilib-utility-standalone/src/validators.ts
+++ wpilib-utility-standalone/src/validators.ts
@@ -1,20 +1,35 @@
 export function hasSpecialCharacter(str: string): boolean {
+  const characterBlacklist = ['@', '!', '.', '$', '&', '|', '`', ':'];
+  // check black list characters
   for (const c of str) {
-    // check number character
+    if (characterBlacklist.indexOf(c) !== -1) {
+      return true;
+    }
+  }
+  // check prefix and suffix
+  for (const c of (str[0] + str[str.length - 1])) {
     if (c >= '0' && c <= '9') {
       continue;
     }
-    // check upper character
     if (c >= 'A' && c <= 'Z') {
       continue;
     }
-    // check lower character
     if (c >= 'a' && c <= 'z') {
       continue;
     }
     return true;
   }
   return false;
+}
+
+export function validatePath(str: string): boolean {
+  const characterBlacklist = ['@', '!', '.', '$', '&', '|', '`'];
+  for (const c of str) {
+    if(characterBlacklist.indexOf(c) !== -1) {
+      return false;
+    }
+  } 
+  return true;
 }
 
 export function validateProject(input: HTMLInputElement, div: HTMLDivElement) {
@@ -40,6 +55,19 @@ export function validateTeamNumber(input: HTMLInputElement, div: HTMLDivElement)
     input.classList.add('error');
   } else {
     div.innerText = 'Enter a team number';
+    div.classList.remove('error');
+    input.classList.remove('error');
+  }
+}
+
+export function validateProjectFolder(input: HTMLInputElement, div: HTMLDivElement) {
+  const s = input.value;
+  if (!validatePath(s)) {
+    div.innerText = 'Select a folder to place the new project into. path includes illegal character(s)';
+    div.classList.add('error');
+    input.classList.add('error');
+  } else {
+    div.innerText = 'Select a folder to place the new project into.';
     div.classList.remove('error');
     input.classList.remove('error');
   }
