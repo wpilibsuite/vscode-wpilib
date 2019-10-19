1dec578a742b6e8c9ea39cd66e8d61939490e2ee
Thad House <thadhouse1@gmail.com>
2019-10-19 12:04:32 -0700

    Only check for 2019 updates when checking for WPILib updates

--- vscode-wpilib/src/wpilibupdates.ts
+++ vscode-wpilib/src/wpilibupdates.ts
@@ -78,7 +78,7 @@ export class WPILibUpdates {
       return false;
     } else {
       const result = await vscode.window.showInformationMessage
-        (`WPILib project update (${newVersion}) found, would you like to install it? ` +
+        (`WPILib project update (${newVersion.newVersion}) found, would you like to install it? ` +
           `${grVersion} currently installed`, {
             modal: true,
           }, 'Yes', 'No');
@@ -177,9 +177,10 @@ export class WPILibUpdates {
         if (versions.length === 0) {
           return undefined;
         }
-        let newestVersion = versions[0];
+        let newestVersion = '0';
+        const frcYear = this.externalApi.getUtilitiesAPI().getFrcYear();
         for (const v of versions) {
-          if (isNewerVersion(v, newestVersion)) {
+          if (v.startsWith(frcYear) && isNewerVersion(v, newestVersion)) {
             newestVersion = v;
           }
         }
