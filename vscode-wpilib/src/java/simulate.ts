8b9143d66a027a096d499554cefc122c4a24de23
Thad House <ThadHouse@users.noreply.github.com>
2019-12-07 17:57:15 -0800

    Fix simulation support on mac (#316)

--- vscode-wpilib/src/java/simulate.ts
+++ vscode-wpilib/src/java/simulate.ts
@@ -1,6 +1,7 @@
 'use strict';
 import * as vscode from 'vscode';
 import { logger } from '../logger';
+import { getIsMac, getIsWindows } from '../utilities';
 
 export interface ISimulateCommands {
   extensions: string;
@@ -11,17 +12,27 @@ export interface ISimulateCommands {
   workspace: vscode.WorkspaceFolder;
 }
 
+interface IEnvMap {
+  // tslint:disable-next-line: no-any
+  [key: string]: any;
+}
+
 export async function startSimulation(commands: ISimulateCommands): Promise<void> {
-  const config: vscode.DebugConfiguration = {
+  const env: IEnvMap = {
+    HALSIM_EXTENSIONS: commands.extensions,
+  };
+  if (getIsWindows()) {
+    env.PATH = commands.librarydir;
+  } else {
+    env.DYLD_LIBRARY_PATH = commands.librarydir;
+    env.LD_LIBRARY_PATH = commands.librarydir;
+  }
+
+  const config = {
     args: commands.robotclass,
     console: 'integratedTerminal',
     cwd: commands.workspace.uri.fsPath,
-    env: {
-      DYLD_LIBRARY_PATH: commands.librarydir,
-      HALSIM_EXTENSIONS: commands.extensions,
-      LD_LIBRARY_PATH: commands.librarydir,
-      PATH: commands.librarydir,
-    },
+    env,
     mainClass: commands.mainclass,
     name: 'WPILib Java Simulate',
     request: 'launch',
@@ -29,6 +40,10 @@ export async function startSimulation(commands: ISimulateCommands): Promise<void
     type: 'java',
     vmArgs: `-Djava.library.path="${commands.librarydir}"`,
   };
+
+  if (getIsMac()) {
+    config.vmArgs += ' -XstartOnFirstThread';
+  }
 
   logger.log('Java Simulation: ', config);
 
