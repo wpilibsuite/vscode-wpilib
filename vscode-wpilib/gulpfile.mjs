1e2f89a2e4701fcafd7b295a3282f18a22244331
GrahamSH <grahamshllk@gmail.com>
2025-06-08 17:58:00 -0400

    update all dependencies

--- vscode-wpilib/gulpfile.mjs
+++ vscode-wpilib/gulpfile.mjs
@@ -3,11 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const gulp = require('gulp');
+import gulp from 'gulp';
 
-const yaml = require('gulp-yaml');
-const del = require('del');
-const nls = require('vscode-nls-dev');
+import yaml from 'gulp-yaml';
+import { deleteSync } from 'del';
+import nls from 'vscode-nls-dev';
 
 // If all VS Code languages are supported you can use nls.coreLanguages
 const languages = [
@@ -16,22 +16,22 @@ const languages = [
   },
 ];
 
-
-gulp.task('i18n-compile', function () {
+gulp.task('i18n-compile', async function () {
   return gulp.src('./locale/**/*.yaml').pipe(yaml()).pipe(gulp.dest('./i18n/'));
 });
 
-gulp.task('i18n-additional', function () {
+gulp.task('i18n-additional', async function () {
   return gulp
     .src(['package.nls.json'])
     .pipe(nls.createAdditionalLanguageFiles(languages, 'i18n'))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('clean', function() {
-	return del(['package.nls.*.json', 'vscode-wpilib*.vsix', 'out/']);
-})
+gulp.task('clean', async function () {
+  return deleteSync(['package.nls.*.json', 'vscode-wpilib*.vsix', 'out/']);
+});
 
 gulp.task('build', gulp.series('clean', 'i18n-compile', 'i18n-additional'));
 
 gulp.task('default', gulp.series('build'));
+
