54a42cef66217abbf32915eda053248557d188d0
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-06-14 22:59:32 -0400

    Refactor gulpfile to use Node built-ins

--- vscode-wpilib/gulpfile.mjs
+++ vscode-wpilib/gulpfile.mjs
@@ -3,10 +3,10 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { globSync } from 'fs';
+import { rm } from 'fs/promises';
 import gulp from 'gulp';
-
 import yaml from 'gulp-yaml';
-import { deleteSync } from 'del';
 import nls from 'vscode-nls-dev';
 
 // If all VS Code languages are supported you can use nls.coreLanguages
@@ -16,22 +16,22 @@ const languages = [
   },
 ];
 
-gulp.task('i18n-compile', async function () {
-  return gulp.src('./locale/**/*.yaml').pipe(yaml()).pipe(gulp.dest('./i18n/'));
-});
+gulp.task('i18n-compile', async () =>
+  gulp.src('./locale/**/*.yaml').pipe(yaml()).pipe(gulp.dest('./i18n/'))
+);
 
-gulp.task('i18n-additional', async function () {
-  return gulp
+gulp.task('i18n-additional', async () =>
+  gulp
     .src(['package.nls.json'])
     .pipe(nls.createAdditionalLanguageFiles(languages, 'i18n'))
-    .pipe(gulp.dest('.'));
-});
+    .pipe(gulp.dest('.'))
+);
 
-gulp.task('clean', async function () {
-  return deleteSync(['package.nls.*.json', 'vscode-wpilib*.vsix', 'out/']);
+gulp.task('clean', async () => {
+  await rm('out/', { force: true, recursive: true });
+  globSync(['package.nls.*.json', '*.vsix']).map(async (file) => await rm(file));
 });
 
 gulp.task('build', gulp.series('clean', 'i18n-compile', 'i18n-additional'));
 
 gulp.task('default', gulp.series('build'));
-
