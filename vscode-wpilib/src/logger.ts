c4afd2cd200fd81dcc3a9ead6527d9e85b110a27
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-11-07 13:54:31 -0500

    Bring back logging to VS Code's Output panel

--- vscode-wpilib/src/logger.ts
+++ vscode-wpilib/src/logger.ts
@@ -1,13 +1,41 @@
 'use strict';
 
 import * as path from 'path';
+import * as vscode from 'vscode';
 import * as winston from 'winston';
+import * as TransportStream from 'winston-transport';
 
 export interface ILogger {
   error(message: string, ...meta: unknown[]): void;
   warn(message: string, ...meta: unknown[]): void;
   info(message: string, ...meta: unknown[]): void;
   log(message: string, ...meta: unknown[]): void;
+}
+
+class VsCodeOutputTransport extends TransportStream {
+  private channel: vscode.LogOutputChannel;
+
+  constructor(opts?: TransportStream.TransportStreamOptions) {
+    super(opts);
+    this.channel = vscode.window.createOutputChannel('WPILib', { log: true });
+  }
+
+  public log(info: winston.LogEntry, next: () => void) {
+    setImmediate(() => this.emit('logged', info));
+    switch (info.level) {
+      case 'error':
+        this.channel.error(info.message, info.meta);
+        break;
+      case 'warn':
+        this.channel.warn(info.message, info.meta);
+        break;
+      // Log everything info and below as info
+      default:
+        this.channel.info(info.message, info.meta);
+        break;
+    }
+    next();
+  }
 }
 
 const myFormat = winston.format.printf((info) => {
@@ -18,7 +46,7 @@ const winstonLogger = winston.createLogger({
   exitOnError: false,
   format: winston.format.combine(winston.format.timestamp(), winston.format.simple(), myFormat),
   level: 'verbose',
-  transports: [new winston.transports.Console()],
+  transports: [new VsCodeOutputTransport()],
 });
 
 export function closeLogger() {
