68f8143b2aad627a39e58a9fd754ecd192dbd94b
sciencewhiz <sciencewhiz@users.noreply.github.com>
2026-08-30 14:47:18 -0700

    Update VS Code used for tests to 1.34.0 to match installer

--- vscode-wpilib/.vscode-test.mjs
+++ vscode-wpilib/.vscode-test.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
   files: 'out/test/**/*.js',
-  version: '1.116.0',
+  version: '1.134.0',
   mocha: {
     timeout: 10000,
   },
